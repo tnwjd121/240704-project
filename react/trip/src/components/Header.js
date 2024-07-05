9ad370816ebd17bbb8c5e8c1cd860294de693dd8
tnwjd121 <aojj121@naver.com>
2024-07-05 15:19:24 +0900

    react menu 및 깨지는 부분 수정하였습니다.

--- react/trip/src/components/Header.js
+++ react/trip/src/components/Header.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import '../css/header.css'
 import { FaRegUserCircle } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
 
 
 export default function Header() {
+  const [menuStates, setMenuStates] = useState({
+    menu1 :false,
+    menu2 :false,
+  });
+
+  const toggleMenu = (menu) => {
+    setMenuStates((prevMenus) => ({
+      ...prevMenus,
+      [menu] : !prevMenus[menu],
+    }))
+  }
   return (
     <header id='header'>
       <div id='Logo'>
@@ -13,14 +25,36 @@ export default function Header() {
       <div id='nav'>
         <nav>
           <ul id='nav-text'>
-            <li><Link to='/test' className='link-style'>테스트 메뉴</Link></li>
-            <li><Link to='/test' className='link-style'>테스트 메뉴</Link></li>
+            <li 
+                onMouseEnter={() => toggleMenu('menu1')}
+                onMouseLeave={() => toggleMenu('menu1')}
+              >
+              <span>정보 등록 <FaChevronDown className='react-icon'/></span>
+              {menuStates.menu1 && (
+              <ul id='side-nav'>
+                <li><Link to='/test' className='link-style'>사이드 메뉴</Link></li>
+                <li><Link to='/test' className='link-style'>사이드 메뉴</Link></li>
+              </ul>
+              )}
+            </li>
+            <li 
+              onMouseEnter={() => toggleMenu('menu2')}
+              onMouseLeave={() => toggleMenu('menu2')}
+            >
+              <span>정보 등록 <FaChevronDown className='react-icon'/></span>
+              {menuStates.menu2 && (
+              <ul id='side-nav'>
+                <li><Link to='/test' className='link-style'>사이드 메뉴</Link></li>
+                <li><Link to='/test' className='link-style'>사이드 메뉴</Link></li>
+              </ul>
+              )}
+            </li>
           </ul>
         </nav>
       </div>
       <div id='user'>
-        <div className='user'><Link to='/login'><FaRegUserCircle/> Login</Link></div>
-        <div className='user link'><Link to='/login'>Join</Link></div>
+        <div className='user'><Link to='/login'><FaRegUserCircle className='react-icon'style={{marginRight:'4px'}}/> Login</Link></div>
+        <div className='user'><Link to='/login'>Join</Link></div>
       </div>
     </header>
   )
--- react/trip/src/pages/Login.js
+++ react/trip/src/pages/Login.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='body'>Login</div>
   )
 }
