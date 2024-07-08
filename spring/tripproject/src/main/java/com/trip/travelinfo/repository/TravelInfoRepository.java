e11e3d8178d0858597be0e68112c54efed429429
hyeongjun <gujuns1987@naver.com>
2024-07-08 10:35:56 +0900

    @CrossOrigin 추가

--- spring/tripproject/src/main/java/com/trip/travelinfo/repository/TravelInfoRepository.java
+++ spring/tripproject/src/main/java/com/trip/travelinfo/repository/TravelInfoRepository.java
@@ -1,9 +1,11 @@
 package com.trip.travelinfo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.trip.travelinfo.entity.TravelInfo;
 
+@CrossOrigin
 public interface TravelInfoRepository extends JpaRepository<TravelInfo, Integer> {
 	
 }
