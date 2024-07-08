ff77b11434aec45ff819d79d013e22aafa551897
SeongBin <rkfmskvos@gmail.com>
2024-07-08 14:59:15 +0900

    no message

--- spring/tripproject/src/main/java/com/trip/userdata/ctrl/UserController.java
+++ spring/tripproject/src/main/java/com/trip/userdata/ctrl/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 	
 	@Autowired
