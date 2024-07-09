d1783137b6fbdcce816e9f48a4a96bf483419002
tnwjd121 <aojj121@naver.com>
2024-07-09 09:30:23 +0900

    trip 삭제

--- spring/trip/src/main/java/com/travelinfo/repository/TravelInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travelinfo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.travelinfo.entity.TravelInfo;
-
-public interface TravelInfoRepository extends JpaRepository<TravelInfo, Integer> {
-	
-}
