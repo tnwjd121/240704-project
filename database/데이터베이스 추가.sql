85d2bd20b61a588ceba0c19658e84740b1ae8294
tnwjd121 <aojj121@naver.com>
2024-07-17 12:18:56 +0900

    배포 파일 생성 및 데이터베이스 추가

---
+++ database/데이터베이스 추가.sql
@@ -0,0 +1,63 @@
+  
+INSERT INTO festival (end_date,fes_id,start_date, detail, address, country, country_name, fes_name, host, image_url, phone_number, price, region)
+VALUES 
+('2024-07-27','1', '2024-07-27', '익사이팅한 음악과 퍼포먼스 그리고 워터파이팅이 더해진 새로운 차원의 경험! 놓칠 수 없는 최고의 워터 뮤직 페스티벌 워터밤 부산 2024', '부산 동구 이순신대로 164', '국내', '대한민국', '워터밤 부산 2024', '', 'https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F018%2F2016%2F03%2F09%2FPS16030900292g_99_20160309110804.jpg&type=sc960_832', '000-0000-0000', '143000', '부산'),
+('2024-10-06', '2', '2024-09-22', '9월말부터 10월초까지 2주동안 열리는 맥주 축제', 'Nordallee 25, 85356 München-Flughafen, 독일', '해외', '독일', '옥토버페스트 축제', '독일', 'https://www.omynara.com/wp-content/uploads/2018/03/Oktoberfest.jpg', '000-0000-0000', '0', '뮌헨'),
+('2024-07-31', '3', '2024-07-13', '이번 <이월드 해바라기 가든 - 부제 : 고흐의 정원>에서는 2천평 포시즌스 가든을 가득 채울 해바라기를 선보인다. 이번 여름은 해바라기와 이국적인 풍경의 이월드를 만나볼 수 있다. 해바라기 가든에는 해바라기와 함께 이월드 페어리들도 찾아온다.', '대구 달서구 두류공원로 200', '국내', '대한민국', '이월드 해바라기 가든', '이월드', 'https://search.pstatic.net/common?type=b&size=672&quality=95&direct=true&src=https%3A%2F%2Fcsearch-phinf.pstatic.net%2F20240704_101%2F1720055506642YRGSl_JPEG%2F3330913_image2_1.jpg', '070-7549-8112', '10000', '대구'),
+('2024-07-21', '4', '2024-06-05', '휴애리 여름 수국축제는 정성스럽게 키운 다양한 수국을 온실, 수국정원, 수국오름 등 공원 곳곳에서 감상할 수 있다. 특별히 온실에서 다양한 수국을 만나 볼 수 있으며 공원 첫 입구부터 나오는 출구까지 전체가 수국으로 덮혀 있다.', '제주특별자치도 서귀포시 남원읍 신례동로 256', '국내', '대한민국', '휴애리 여름 수국축제', '휴애리', 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA2MTdfMjE2%2FMDAxNjg2OTM0MzYyOTgy.Y9xO61QDlcVkATa-OTpXlD_y228a4tZmISmOofnVC9Yg.9kYz1D-GhKq44M0Fjb24akwFqq-IpE9ZmjnVO7ku3R0g.JPEG.yellowjin98%2FSUN02390.jpg&type=sc960_832', '064-732-2114', '13000', '제주도'),
+('2024-09-29', '5', '2024-07-12', '문화예술축제', '강원특별자치도 영월군 영월읍 영월로 1909-10', '국내', '대한민국', '동강국제사진제', '동강사진박물관', 'https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5038%2F2019%2F07%2F12%2F0000509788_003_20190712213214551.jpg&type=sc960_832', '033-375-4554', '2000', '강원도'),
+('2024-08-04', '7', '2024-07-26', '평창더위사냥축제는 대한민국 대표 여름맞이 축제이다. 연중 10℃의 시원한 냉천수가 흐르는 땀띠공원에서 다양한 체험프로그램과 공연으로 더위에 지친 몸과 마음을 쉬어갈 수 있다.', '강원특별자치도 평창군 대화면 대화5길 2', '국내', '대한민국', '평창더위사냥축제', '평창더위사냥축제위원회', 'https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5038%2F2019%2F07%2F27%2F0000512238_001_20190727193815214.jpg&type=sc960_832', '000-0000-0000', '15000', '강원도'),
+('2024-07-27', '8', '2024-07-25', '하이트진로 공장 전주점에서 당일 생산한 맥주를 공급 받아 전국에서 가장 신선한 맥주를 선보이는 \'전주가맥축제\'. 전주 현지인들이 인정하는 가맥업소들과 화려한 공연이 한 데 어우러져 7월 25일부터 27일까지 3일간 시원하고 짜릿한 여름밤을 선사한다.', '전북특별자치도 전주시 덕진구 기린대로 451', '국내', '대한민국', '전주 가맥축제', '전주가맥축제추진위원회', 'https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F015%2F2018%2F08%2F09%2F0003993441_002_20180809212906928.jpg&type=sc960_832', '000-0000-0000', '1000', '전북특별자치도'),
+('2024-07-07', '13', '2024-07-03', '매년 여름, 대구에서 치킨과 맥주의 조합을 테마로 한 대구치맥페스티벌이 열린다. 치맥페스티벌이라는 말 그대로 축제 기간 동안 치킨과 시원한 맥주를 즐기며 가수들의 공연을 관람할 수 있다.', '대구 달서구 공원순환로 36', '국내', '대한민국', '대구치맥페스티벌', '두류공원', 'https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5324%2F2018%2F07%2F04%2F0000169004_001_20180704191050718.jpg&type=sc960_832', '000-0000-0000', '0', '대구'),
+('2024-07-15', '14', '2024-07-01', '하카타를 대표하는 축제', '하카타 기온 야마카사 일본 〒812-0026 Fukuoka, Hakata Ward, Kamikawabatamachi, 1−8', '해외', '일본', '하카타 기온 야마카사', '하카타', 'https://digjapan.travel/files/topics/8319_ext_02_0.jpg', '000-0000-0000', '0', '하카타'),
+('2024-07-31', '15', '2024-07-07', '연극, 무용, 음악의 국제적인 축제', 'Pl. du Palais, 84000 Avignon, 프랑스', '해외', '프랑스', '아비뇽 페스티벌', '아비뇽', 'https://i.namu.wiki/i/vkv0kvaMLj4gAGLAS0PSHdzzk0ELMUazVSmiPJBzeQSqiA3oi-BldeiWqSuT-ExzRMWyvkNhJze2Ymg_i5sdQQ.webp', '000-0000-0000', '70000', '아비뇽'),
+('2024-07-14', '16', '2024-07-11', '다양한 음악 장르, 문화 활동 및 캠핑의 전자 음악 축제', 'Polgar Andraša 1, Novi Sad 21000 세르비아', '해외', '세르비아', '엑시트 페스티벌', '노비사드', 'https://djmagasia.com/wp-content/uploads/2020/06/exit-festival-2020-album-6-1024x853.jpg', '000-0000-0000', '100000', '구차');
+select * from festival;
+
+select * from travel_info;
+
+INSERT INTO travel_info(id, category, country, region, address, place_name, description, photo_url)
+values
+('1','자연명소', '대한민국', '부산', '부산 수영구 광안해변로 219', '광안리해수욕장', '반월형으로 휘어진 사장과 깨끗한 수질을 자랑하는 광안리해수욕장은 부산의 대표적인 도심 속의 해양공원이다. 총면적 82,000㎡에 길이 1.4km, 25~110m의 모래사장이 있다. ', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20190130_267%2F1548841089253xWQyR_JPEG%2F5965uL51HLf9GYmJp9KUm4jq.jpeg.jpg'),
+('2', '체험관광', '대한민국', '부산', '부산 서구 송도해변로 171', '송도해상케이블카', '부산 송도해수욕장에 위치한 송도해상케이블카는 송도해수욕장 동쪽 송림공원에서 서쪽 암남공원까지 1.62km 바다 위를 가로질러 운행합니다. 최고 86m 높이의 바다 한가운데에서 하늘을 날으는 짜릿함과 동시에 송도해수욕장, 부산 영도와 남항대교, 송도용궁구름다리, 파도치는 기암 절벽까지 한 눈에 감상할 수 있습니다', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fnaverbooking-phinf.pstatic.net%2F20220824_156%2F1661314945460uMOcS_JPEG%2F3.jpg'),
+('3', '문화유적', '대한민국', '부산', '부산 동구 홍곡로 75', '문화공감수정', '일본식 가옥, 문화재', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20181119_14%2F154262053839870NDO_JPEG%2FZ1w9sl6qvrcUZXeQDIPn5oer.jpg'),
+('4', '음식점(카페)', '대한민국', '부산', '부산 부산진구 서전로68번길 52', '페이버 커피', '2층', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20221119_2%2F1668834993542M2Vo0_JPEG%2FB3B14330-2EED-4621-AA9E-2C05723EE367.jpeg'),
+('5', '문화유적', '대한민국', '서울', '서울 종로구 사직로 161', '경복궁', '서울특별시 종로구 세종로에 있는 조선전기에 창건되어 정궁으로 이용된 궁궐', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200515_220%2F1589538737340ExaaP_JPEG%2F6aNWC5ueurymWqn68Ics8vcF.jpg'),
+('6', '레저', '대한민국', '서울', '서울 은평구 은평로 123', '에스보라카이', '1405호, 24시간 영업', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20161006_102%2F1475731181459O4J8a_JPEG%2F176879617028599_0.jpeg'),
+('7', '레저', '대한민국', '서울', '서울 서초구 올림픽대로 2085-14', '골든블루마리나', '세빛섬 골든블루 마리나', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20190328_188%2F1553777862026FudVv_JPEG%2F%25B0%25F1%25B5%25E7%25BA%25ED%25B7%25E7%25B8%25B6%25B8%25AE%25B3%25AA_3.jpg'),
+('8', '체험관광', '대한민국', '서울', '서울 강북구 화계사길 117', '화계사 템플스테이템', '템플스테이 체험', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20170228_67%2F1488265432539DibeE_JPEG%2F04302049.JPG'),
+('9', '체험관광', '대한민국', '서울', '서울 용산구 남산공원길 105', 'N서울타워', '10:30분 운영 시작', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fnaverbooking-phinf.pstatic.net%2F20230406_290%2F1680769347720J8bJb_JPEG%2F4.jpg'),
+('10', '체험관광', '대한민국', '서울', '서울 중랑구 망우로70길 118', '용마랜드', '폐놀이공원', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20231113_136%2F1699858492287VAXHP_PNG%2FKakaoTalk_20231113_154832697.png'),
+('11', '공연(전시)', '대한민국', '서울', '서울 종로구 삼일대로 461', '아트보다', '11:00 운영 시작', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210823_258%2F1629696948204pEAYB_JPEG%2FDvuWU3PV0t4pXlKUVaPGHhbm.jpg'),
+('12', '공연(전시)', '대한민국', '서울', '서울 송파구 올림픽로 300', '롯데콘서트홀', '공연장', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20160518_263%2F1463565286291bbu4J_PNG%2F176468588464069_0.png'),
+('13', '음식점(카페)', '대한민국', '서울', '서울 성동구 상원1길 5', '브레디포스트 성수점', '1층', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20231013_18%2F1697176172720NatgK_PNG%2F%25BD%25BA%25C5%25A9%25B8%25B0%25BC%25A6_2023-10-13_%25BF%25C0%25C8%25C4_1.10.23.png'),
+('14', '레저', '대한민국', '대구', '대구 중구 동성로3길 83', '레이저아레나 대구', '2층', 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230111_276%2F1673427504048lJ8Rw_JPEG%2FA7M00038.jpg'),
+('15', '체험관광', '일본', '후쿠오카', '2 Chome-902-1 Momochihama, Sawara Ward, Fukuoka, 814-0001 일본', '시사이드 모모치 해변공원', '인공 섬을 따라 난 인기 있는 인공 해변으로, 길게 뻗은 모래사장에서 발리볼을 즐길 수 있습니다.', 'https://lh5.googleusercontent.com/p/AF1QipODcog2Q9ZWXu3Bhqjona6JgmhdNvkgNu8-SnTi=w426-h240-k-no'),
+('16', '음식점(카페)', '일본', '후쿠오카', '일본 〒104-0061 Tokyo, Chuo City, Ginza, 2 Chome−14−5 Twg銀座イースト 1階', '소금빵 팡 메종 긴자점', '소금빵', 'https://lh5.googleusercontent.com/p/AF1QipOdCKlQxEOLquW0unijN8qifehDoC_d6mkcmfMZ=w408-h306-k-no'),
+('17', '음식점(카페)', '일본', '도쿄', '일본 〒104-0061 Tokyo, Chuo City, Ginza, 5 Chome−2−1 東急プラザ銀座 10階', '네무로하나마루 긴자점', '초밥', 'https://lh5.googleusercontent.com/p/AF1QipOIQi7knRg7f4AVXRzPJA-rySO0KeuVPE626bup=w408-h306-k-no'),
+('18', '음식점(카페)', '대만', '타이베이', 'No. 8-1號, Emei St, Wanhua District, Taipei City, 대만 108', '아종면선', '국수 전문점', 'https://lh5.googleusercontent.com/p/AF1QipOsDb1q9VAkzAozY2KkfEegQJUitAFJqYTaiNel=w408-h306-k-no'),
+('19', '체험관광', '대만', '신베이', 'Jishan St, Ruifang District, New Taipei City, 대만 224', '지우펀', '좁은 골목길과 노점, 탁 트인 산과 바다 전망으로 유명한 상업 지구입니다.', 'https://lh5.googleusercontent.com/p/AF1QipObuP2KEnagzY9Han0shUxcN2RYRHDgUbNOHBLx=w408-h247-k-no'),
+('20', '체험관광', '미국', '뉴욕', '미국 10036 New York, Manhattan', '타임즈 스퀘어', '밝은 조명, 쇼핑, 쇼 등으로 유명한 극장가 중심부에 위치한 활기 넘치는 곳입니다.', 'https://lh5.googleusercontent.com/p/AF1QipMBjD8tf3BVdTpg-RhIL2XXDzdnMKc-UnoWUxlI=w408-h544-k-no'),
+('21', '문화유적', '영국', '런던', '영국 SW1A 1AA London, 런던', '버킹엄 궁전', '궁전의 화려한 방과 접견실을 둘러보거나 경비병의 교대 의식을 지켜볼 수 있습니다.', 'https://lh5.googleusercontent.com/p/AF1QipP3AiS_2pee3CMhtlGwRtatocM44CWBg4n4tGaw=w408-h306-k-no'),
+('22', '체험관광', '영국', '런던', 'Great Russell St, London WC1B 3DG 영국', '영국 박물관', '이집트 미라, 고대 그리스 조각상을 비롯한 세계 유물의 거대한 전시장입니다.', 'https://lh5.googleusercontent.com/p/AF1QipPJA1W3rAPK3xAArAOtYdTMu5rgRDbkTAotGV3t=w408-h306-k-no');
+
+select * from user;
+
+insert into user(age,gender, email, id, name, password)
+values
+('20', 1, 'abcd@naver.com', 'abcde', '홍길동', '1234'),
+('25', 0, 'aojj121@naver.com', 'aojj121', '백수정', '1234'),
+('6', 1, 'ddd@naver.com', 'dkjkjdk1214', '가나다', '1234'),
+('22', 1, 'qwer@naver.com', 'qwer123', '가나다', '1234');
+
+select * from review;
+insert into review(score, travel_info_id, id, contents, user_id)
+values
+('4', '10', '4', '사진 찍기 너무 좋아요!', 'aojj121'),
+('3', '3', '5', '일본식 가옥이 너무 이뻐요', 'aojj121'),
+('5', '8', '6', '제가 간 템플스테이 중 제일 좋아요', 'aojj121'),
+('5', '7', '7', '야경이 좋습니다.', 'aojj121'),
+('4', '7', '8', '또 가고 싶어요!', 'abcde'),
+('4', '18', '9', '너무 맛있어요!', 'abcde'),
+('2', '19', '10', '냄새가 심해요ㅜㅜ', 'abcde'),
+('4', '19', '11', '야경이 이뻐요!', 'qwer123');
